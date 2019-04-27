1cc6602930bf7aba655d48b34f66207546ab414d
li_jerry <lijerrymagic@gmail.com>
2019-04-27 15:02:12 -0400

    add ssl

--- app.js
+++ app.js
@@ -4,22 +4,28 @@ const https = require('https');
 const http = require('http');
 const app = express()
 
+// Constants
+const HTTPS_PORT = process.env.PORT | 3000;
+const HTTP_PORT = 80
+
 app.use(function (req, res, next){
+	console.log(req.protocol);
+	if (req.protocol === 'http'){
+		res.redirect(301, 'https://' + req.host  + ":" + HTTPS_PORT + req.url);
+		console.log("HTTPS request", req.method, req.url, req.body);
+	}
 	return next();
 });
 
-// Constants
-const PORT = 3000;
-
-// let httpsOptions = {
-//    cert:fs.readFileSync('./ssl/www_zeyuli_me.crt'),
-//    ca:fs.readFileSync('./ssl/www_zeyuli_me.ca-bundle'),
-//    key:fs.readFileSync('./ssl/zeyuli_me.key')
-// };
-// app.get('/', (req, res) => res.redirect('/index.html'))
+let httpsOptions = {
+   cert:fs.readFileSync('./ssl/www_zeyuli_me.crt'),
+   ca:fs.readFileSync('./ssl/www_zeyuli_me.ca-bundle'),
+   key:fs.readFileSync('./ssl/zeyuli_me.key')
+};
 
 app.use(express.static('frontend'));
 
-http.createServer(app).listen(PORT, function () {
-    console.log('HTTP on port 3000');
+https.createServer(httpsOptions,app).listen(HTTPS_PORT, function () {
+    console.log('HTTPS on port 3000');
 });
+http.createServer(app).listen(HTTP_PORT);
